fb53db9d09cbc40f9da46c24d0c458a70e0db187
jchristopherson <jchristopherson@hotmail.com>
2021-12-06 14:53:46 -0600

    Update interface

--- src/CoulombModels.jl
+++ src/CoulombModels.jl
@@ -3,11 +3,11 @@
 """
 Applies Coulomb's model to compute the friction force at the defined state.
 """
-function friction(mdl::CoulombModel, nrm::Number, vel::Number, p::Number...)
+function friction(mdl::CoulombModel, nrm::Number, vel::Number; p...)
     if vel == 0.0
         F = mdl.coefficient * nrm
     else
         F = -mdl.coefficient * nrm * sign(vel)
     end
-    return (force = F, params = ())
+    return (f = F, params = ())
 end
